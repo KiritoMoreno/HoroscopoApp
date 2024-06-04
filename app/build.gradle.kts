3911b7cabbdc20ff694d9ed7ae474bb6e46947b6
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-06-04 19:08:59 +0200

    Agregamos la libreria ( Navigation component ) Es una libreria que nos  simplifica y nos gestiona toda la nevagacion entre fragment y activity De una forma segura, sencilla y comoda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 
 dependencies {
 
+    val navVersion  = "2.7.1"
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -47,4 +49,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    //
+
 }
