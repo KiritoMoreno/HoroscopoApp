e7d2171ee6e4ed6cf3acd870d41b3a4c2edfe57d
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-06-06 17:18:02 +0200

    Creamos un modelo en la carpeta domain ya que no va a cambiar los datos y no tenemos necesidad de hacer un modelo por capa

--- app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopeFragment.kt
@@ -11,7 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
-    
+
     private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
 
     private var _binding: FragmentHoroscopeBinding? = null
