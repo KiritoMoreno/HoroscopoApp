c31a4026f01e2cfe7580aaa5ccd2ffa3ecadce6b
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-06-07 13:59:05 +0200

    Creamos una couritine especial que se enganchara al ciclo de vida del fragmento

--- app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,13 +1,18 @@
 package com.example.horoscopapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.horoscopapp.databinding.FragmentHoroscopeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
@@ -17,9 +22,26 @@ class HoroscopeFragment : Fragment() {
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
 
-    
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        // Creamos una courutine especial, esta se engancha al ciclo de vida del fragmento
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect {
+                    Log.i("M.Funciona", it.toString())
+                }
+            }
+        }
 
     }
 
