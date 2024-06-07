dce12d87f67c43e18e81b0a18a8aa15ea6c17fa5
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-06-07 14:19:42 +0200

    Agregamos el item de horoscope y la logica del adapter y el viewholder

--- app/src/main/java/com/example/horoscopapp/ui/horoscope/adapter/HoroscopeAdapter.kt
+++ app/src/main/java/com/example/horoscopapp/ui/horoscope/adapter/HoroscopeAdapter.kt
@@ -1,4 +1,23 @@
 package com.example.horoscopapp.ui.horoscope.adapter
 
-class HoroscopeAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.horoscopapp.R
+import com.example.horoscopapp.domain.HoroscopeInfo
+import com.example.horoscopapp.ui.horoscope.HoroscopeViewModel
+
+class HoroscopeAdapter (private var horoscopeList: List<HoroscopeInfo> = emptyList()): RecyclerView.Adapter<HoroscopeViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HoroscopeViewHolder {
+        return HoroscopeViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_horoscope,parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = horoscopeList.size
+
+    override fun onBindViewHolder(holder: HoroscopeViewHolder, position: Int) {
+        holder.render(horoscopeList[position])
+    }
+
 }
