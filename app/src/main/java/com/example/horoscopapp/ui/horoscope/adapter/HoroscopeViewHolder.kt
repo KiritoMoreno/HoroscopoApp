8b670a155a213bd54f678453cdaf5967775e85eb
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-06-07 16:12:05 +0200

    Creamos nuestra animation rotation

--- app/src/main/java/com/example/horoscopapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
+++ app/src/main/java/com/example/horoscopapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.horoscopapp.ui.horoscope.adapter
 
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.example.horoscopapp.databinding.ItemHoroscopeBinding
 import com.example.horoscopapp.domain.HoroscopeInfo
@@ -15,6 +16,18 @@ class HoroscopeViewHolder (view: View): RecyclerView.ViewHolder(view){
         binding.tvTitle.text = context.getString(horoscopeInfo.name)
 
         // le pasamos la funcion lambda  y dentro el item seleccionado
-        binding.itemHoroscopeParent.setOnClickListener { onItemSelected(horoscopeInfo) }
+        binding.itemHoroscopeParent.setOnClickListener {
+            startRotationAnimation(binding.ivHoroscope)
+            // onItemSelected(horoscopeInfo)
+        }
+    }
+
+    private fun startRotationAnimation(view: View){
+        view.animate().apply{
+            duration = 500
+            interpolator = LinearInterpolator()  // Va a tener la misma velocidad desde el inicio hasta el final con linearInterpolator
+            rotationBy(360f)  // Rotar sobre su propio punto
+            start()
+        }
     }
 }
