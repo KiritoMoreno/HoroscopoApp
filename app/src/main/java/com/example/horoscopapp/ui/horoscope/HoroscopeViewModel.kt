643b46b3ae5022822076f79e1e1c3b0e4e5b75a1
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-06-07 13:45:47 +0200

    Usamos un StateFlow que esta enfocado en las vistas, ya que puede tener diferentes estados

--- app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopeViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.horoscopapp.ui.horoscope
 
 import androidx.lifecycle.ViewModel
+import com.example.horoscopapp.domain.HoroscopeInfo
+import com.example.horoscopapp.domain.HoroscopeInfo.*
+
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeViewModel @Inject constructor():ViewModel() {
+class HoroscopeViewModel @Inject constructor() : ViewModel() {
+
+    private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope: StateFlow<List<HoroscopeInfo>> = _horoscope
+
+    init {
+        _horoscope.value = listOf(
+            Aries, Taurus, Gemini
+        )
+    }
 }
